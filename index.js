a09a32fd4f903a2e5becbc1e90b8aaa9e65a6372
VinitMakasare <110718213+VinitMakasare@users.noreply.github.com>
2023-01-20 12:33:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+for(i=0; i<7; i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        var buttonPressed = this.innerHTML;
+        makeSound(buttonPressed); 
+        buttonAnimation(buttonPressed);
+    });       
+}
+
+document.addEventListener("keypress",function(event){
+makeSound(event.key);
+buttonAnimation(event.key);
+});
+  
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();             
+            break;
+            
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();            
+            break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();            
+            break;
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();            
+            break;
+
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();            
+            break;
+
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();            
+            break;
+
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();            
+            break;
+          
+      
+        default:
+            break;
+      }
+
+
+}
+
+
+function buttonAnimation(keypress){
+    var activeButton = document.querySelector("."+keypress);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    },100);
+}
+
+
+
+
+
+
+
+
+// //calculator
+
+// function add(num1,num2){
+//     console.log(num1+num2);
+// }
+
+// function multiply(num1,num2){
+//     console.log(num1*num2);
+// }
+// function substract(num1,num2){
+//     console.log(num1-num2);
+// }
+
+// function calculator(num1,num2,oper){
+//     console.log(oper(num1,num2));
+// }
+
+// calculator(2,3,substract);
